dbebc1c516313c52d0a4e2d1f070c6143671b860
jack-wang128801 <1632672310@qq.com>
2019-06-23 23:43:36 +0800

    review

--- cpp_code_review/实现String/实现String/Istring.cpp
+++ cpp_code_review/实现String/实现String/Istring.cpp
@@ -1,25 +1,87 @@
 
-#define _CRT_CESURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
-
+#include<string>
+#include<string.h>
 using namespace std;
 
 class String{
+	friend ostream &operator<<(ostream &_cout, const String& s);
 public:
-	String(char *str=" ")
-		:_size(0)
-		, _capacity(0)
-		, _str()
+	String(char *str="")
+		:_size(strlen(str))
+		, _capacity(_size*2)
+		, _str(new char[strlen(str)+1])
 	{
-
+		strcpy(_str, str);
 	}
+	~String(){
+		if (_str){
+			delete[] _str;
+			_str = nullptr;
+		}
+	}
+
+	String(const String& s)
+	{
+		String tmp(s._str);
+		swap(_str,tmp._str);
+	}
+	String &operator=(const String& s){
+		String tmp(s);
+		swap(_str, tmp._str);
+		return *this;
+	}
+
+	char* c_str(){
+		return _str;
+	}
+	size_t Size(){
+		return _size;
+	}
+
+	void PushBack(const char c){
+		if (_size == _capacity){
+			size_t newcapacity = (_capacity == 0) ? 15 : 2 * _capacity;
+			Reverse(newcapacity);
+		}
+		_str[++_size] = c;
+		_str[_size] = '\0';
+	}
+
+	//增容的逻辑
+	void Reverse(size_t newsize){
+		if (newsize > _capacity){
+			char* tmp = new char[newsize + 1];
+			strcpy(tmp, _str);
+			delete[] _str;
+			_str = tmp;
+			_capacity = newsize;
+		}
+	}
+
 private:
-	char* _str;
 	size_t _size;
 	size_t _capacity;
+	char* _str;
 };
 
-int main(){
 
+ostream &operator<<(ostream &_cout, const String& s){
+	_cout << s._str;
+	return _cout;
+}
+
+void testString(){
+	
+}
+int main(){
+	//testString();
+	String s("hello bit!");
+	cout << s.Size() << endl;
+	String s1 = s;
+	//两者对比
+	cout << s1.Size() << endl;
+	
 	return 0;
 }
---
+++ cpp_code_review/实现String/实现String/const.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//
+//using namespace std;
+//class Const{
+//public:
+//	 int Add( int a, int b) const{
+//		return a + b;
+//	}
+//};
+//
+//int main(){
+//	Const s;
+//	cout <<(s.Add(1, 2)=2<< endl;
+//	return 0;
+//}
